6f37413d8fedc6f7acf2e4f31daf61553154a055
Harsh <harshrawat096@gmail.com>
2023-01-23 22:09:33 +0530

    updated

--- books/urls.py
+++ books/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('books/',include('books_api')),
+    path('books/',include('books_api.urls')),
 ]
